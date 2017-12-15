de972bec9be51b1665394866617313dc88c10322
goborn <garry.oborn@365smartshop.com>
2017-12-14 18:47:49 -0700

    import findProjectFeatures

---
+++ src/components/content/project_components/Features/FeaturesExample.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import ProjectSetupSidebar from '../ProjectSetupSidebar/ProjectSetupSidebar';
+import './features.scss';
+import Header from '../../../Header/Header';
+import { findProjectFeatures } from '../../../../services/project.feature.services';
+
+
+class Features extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="main-fix">
+                    <ProjectSetupSidebar userid={userid} projectid={projectid}/>
+                    <div className="features-container">
+                        <div className="container-wrapper">
+                            <div className="project-section-header">
+                                <label>Features</label>
+                            </div>
+                                <div className="features-area drop-shadow">
+                                    <div className="features-wrapper">
+                                        <div className="features-list">
+
+                                        </div>
+                                        <div className="features-footer">
+                                        <button className="add-button"> <span/> Add Feature </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Features;
