641aa8524b140062254120c2d83e1f69b2a5daf0
goborn <garry.oborn@365smartshop.com>
2017-12-19 19:21:01 -0700

    Moved view field div from view.js into viewfield.js

--- src/components/content/project_components/View/View.js
+++ src/components/content/project_components/View/View.js
@@ -110,15 +110,7 @@ class View extends Component {
     const displayViews = views.map( view => {
         const index = views.indexOf(view);
         return (
-          <div className="view-item" key={`idea-${index}`}>
-          <section>
-            <label>{(index + 1) + '.'}</label>
-            <input className="view-input-name" type="text" id={view.id} value={view.name} onChange={e => this.handleChangeView(e, index, "name")} />
-            <input className="view-input-imgurl" type="text" id={view.id} value={view.image_url} onChange={e => this.handleChangeView(e, index, "image_url")} />
-          </section>
-            <button  id={view.id} onClick={e => this.submitChangeView(e, index)}>Save</button>
-            <span className="delete-x" id={view.id} onClick={e => this.handleDeleteViewButton(e, index)}>&times;</span> 
-        </div>
+          
         )
     });
 
--- src/components/content/project_components/View/ViewItems/ViewField.js
+++ src/components/content/project_components/View/ViewItems/ViewField.js
@@ -8,7 +8,17 @@ class Viewfield extends Component {
     }
 
     render() {
-        return ();
+        return (
+            <div className="view-item" key={`idea-${index}`}>
+            <section>
+              <label>{(index + 1) + '.'}</label>
+              <input className="view-input-name" type="text" id={view.id} value={view.name} onChange={e => this.handleChangeView(e, index, "name")} />
+              <input className="view-input-imgurl" type="text" id={view.id} value={view.image_url} onChange={e => this.handleChangeView(e, index, "image_url")} />
+            </section>
+              <button  id={view.id} onClick={e => this.submitChangeView(e, index)}>Save</button>
+              <button className="delete-x" id={view.id} onClick={e => this.handleDeleteViewButton(e, index)}>&times;</button> 
+          </div>
+        );
     }
 
 }
