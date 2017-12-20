7f8435af6bed922dee88f7a58470a55a247fb133
goborn <garry.oborn@365smartshop.com>
2017-12-19 19:31:09 -0700

    adding const variable to viewField.js to pass data from view.js down to viewfield

--- src/components/content/project_components/View/ViewItems/ViewField.js
+++ src/components/content/project_components/View/ViewItems/ViewField.js
@@ -8,15 +8,16 @@ class Viewfield extends Component {
     }
 
     render() {
+        const { index, viewid } = this.props;
         return (
-            <div className="view-item" key={`idea-${index}`}>
+            <div className="view-item">
             <section>
               <label>{(index + 1) + '.'}</label>
-              <input className="view-input-name" type="text" id={view.id} value={view.name} onChange={e => this.handleChangeView(e, index, "name")} />
-              <input className="view-input-imgurl" type="text" id={view.id} value={view.image_url} onChange={e => this.handleChangeView(e, index, "image_url")} />
+              <input className="view-input-name" type="text" id={viewid} value={view.name} onChange={e => this.handleChangeView(e, index, "name")} />
+              <input className="view-input-imgurl" type="text" id={viewid} value={view.image_url} onChange={e => this.handleChangeView(e, index, "image_url")} />
             </section>
-              <button  id={view.id} onClick={e => this.submitChangeView(e, index)}>Save</button>
-              <button className="delete-x" id={view.id} onClick={e => this.handleDeleteViewButton(e, index)}>&times;</button> 
+              <button  id={viewid} onClick={e => this.submitChangeView(e, index)}>Save</button>
+              <button className="delete-x" id={viewid} onClick={e => this.handleDeleteViewButton(e, index)}>&times;</button> 
           </div>
         );
     }
