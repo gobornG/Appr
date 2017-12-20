619d5ed1a7cdb45c095650b51a5b13fbf539337e
goborn <garry.oborn@365smartshop.com>
2017-12-19 19:34:38 -0700

    Changed ImageURL variable

--- src/components/content/project_components/View/ViewItems/ViewField.js
+++ src/components/content/project_components/View/ViewItems/ViewField.js
@@ -8,13 +8,13 @@ class Viewfield extends Component {
     }
 
     render() {
-        const { index, viewid, viewName } = this.props;
+        const { index, viewid, viewName, imgURL } = this.props;
         return (
             <div className="view-item">
             <section>
               <label>{(index + 1) + '.'}</label>
               <input className="view-input-name" type="text" id={viewid} value={viewName} onChange={e => this.handleChangeView(e, index, "name")} />
-              <input className="view-input-imgurl" type="text" id={viewid} value={view.image_url} onChange={e => this.handleChangeView(e, index, "image_url")} />
+              <input className="view-input-imgurl" type="text" id={viewid} value={imgURL} onChange={e => this.handleChangeView(e, index, "image_url")} />
             </section>
               <button  id={viewid} onClick={e => this.submitChangeView(e, index)}>Save</button>
               <button className="delete-x" id={viewid} onClick={e => this.handleDeleteViewButton(e, index)}>&times;</button> 
