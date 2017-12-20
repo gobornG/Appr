47f2341cea6661c167e3a931c61c3e2f3a23946e
goborn <garry.oborn@365smartshop.com>
2017-12-19 19:32:52 -0700

    added viewName

--- src/components/content/project_components/View/ViewItems/ViewField.js
+++ src/components/content/project_components/View/ViewItems/ViewField.js
@@ -8,12 +8,12 @@ class Viewfield extends Component {
     }
 
     render() {
-        const { index, viewid } = this.props;
+        const { index, viewid, viewName } = this.props;
         return (
             <div className="view-item">
             <section>
               <label>{(index + 1) + '.'}</label>
-              <input className="view-input-name" type="text" id={viewid} value={view.name} onChange={e => this.handleChangeView(e, index, "name")} />
+              <input className="view-input-name" type="text" id={viewid} value={viewName} onChange={e => this.handleChangeView(e, index, "name")} />
               <input className="view-input-imgurl" type="text" id={viewid} value={view.image_url} onChange={e => this.handleChangeView(e, index, "image_url")} />
             </section>
               <button  id={viewid} onClick={e => this.submitChangeView(e, index)}>Save</button>
